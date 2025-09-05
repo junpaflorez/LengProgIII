baa418c6ae51e1739a2f3a6252d5b2396c0f3267
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2025-09-04 20:30:54 -0500

    Update Practica1.java

--- Practica1.java
+++ Practica1.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Practica1{
 
@@ -33,7 +34,7 @@ public class Practica1{
         System.out.println("Soy un metodo estatico");
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         System.out.println("hola mundo");
         Practica1 practica1 = new Practica1();
         practica1.saludo();
@@ -48,13 +49,23 @@ public class Practica1{
         System.out.println();
         Conejo rabbit = new Conejo();
         rabbit.pintarConejo();
-
+        /*
         rabbit.cambiarCara("(=O.o=)");
         System.out.println("Escribe tu nombre para ser saludado");
         Scanner scanner = new Scanner(System.in);
         String nombre = scanner.nextLine();
+        
         rabbit.saludo(nombre);
         rabbit.pintarConejo();
+        */
+        for (int i = 0; i < 100; i++) {
+            rabbit.cambiarCara("(=O.o=)");
+            rabbit.pintarConejo();
+            TimeUnit.SECONDS.sleep(1);
+            rabbit.cambiarCara(" (=o.O=)");
+            rabbit.pintarConejo();
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 
 }
